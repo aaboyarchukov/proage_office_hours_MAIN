80d3f61af65333c5ae0cd1d9084d2892f09bf26b
aaboyarchukov <101995272+aaboyarchukov@users.noreply.github.com>
2024-11-07 19:01:52 +0300

    Update useSlotID.js

--- src/hooks/useSlotID.js
+++ src/hooks/useSlotID.js
@@ -1,7 +1,7 @@
 // Desc: Hook to get and save the slot ID from or to the local storage
 import useLocalStorageState from 'use-local-storage-state'
 function useSlotID() {
-    const [slotID, setSlotID] = useLocalStorageState('slotID', { defaultValue: -1 });
+    const [slotID, setSlotID] = useLocalStorageState('slotID', { defaultValue: "-1" });
 
     return [slotID, setSlotID];
 
